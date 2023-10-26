f5af1998dbbedcd41e43e497bf48f176f0c22af9
hoang1803 <21020073@vnu.edu.vn>
2023-10-26 17:34:41 +0700

    Update chapter.dart

--- signlingo/lib/home/chapter.dart
+++ signlingo/lib/home/chapter.dart
@@ -68,7 +68,7 @@ class _ChapterHeaderState extends State<_ChapterHeader> {
     return Container(
       height: 51.0,
       color: Colors.white,
-      child: Padding(   
+      child: Padding(
         padding: const EdgeInsets.only(top: 0),
         child: Row(
           mainAxisSize: MainAxisSize.max,
@@ -110,7 +110,7 @@ class _ChapterHeaderState extends State<_ChapterHeader> {
                         backgroundColor: Colors.black26,
                         valueColor:
                             AlwaysStoppedAnimation(Colors.amber.shade600),
-                        value: widget.progress,
+                        value: widget.progress / 100,
                       ),
                     ),
                     Center(
@@ -126,6 +126,67 @@ class _ChapterHeaderState extends State<_ChapterHeader> {
                 ))
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _ChapterBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ChapterBodyState();
+  }
+}
+
+class _ChapterBodyState extends State<_ChapterBody> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _ChapterBodyItem extends StatefulWidget {
+  final String name;
+  bool hasComplete;
+  _ChapterBodyItem({required this.name, required this.hasComplete});
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ChapterBodyItemState();
+  }
+}
+
+class _ChapterBodyItemState extends State<_ChapterBodyItem> {
+  final List<Icon> iconList = [
+    const Icon(Icons.remove_red_eye_outlined,
+        size: 40.0, color: Colors.orange, semanticLabel: 'Discover'),
+    const Icon(Icons.mode_comment_outlined,
+        size: 40.0, color: Colors.orange, semanticLabel: 'Dialog'),
+    const Icon(Icons.rate_review_outlined,
+        size: 40.0, color: Colors.orange, semanticLabel: 'Review'),
+    const Icon(Icons.picture_in_picture_alt_outlined,
+        size: 40.0, color: Colors.orange, semanticLabel: 'Iconic')
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: 60.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          iconList[iconList.indexWhere((element) {
+            Icon icon = element as Icon;
+            return icon.semanticLabel == widget.name;
+          })],
+          Text(
+            widget.name,
+            textAlign: TextAlign.justify,
+          )
+        ],
       ),
     );
   }
