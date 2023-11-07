0adaa333cab8b3182c03d1c40510ef9e9b0f411a
hoang1803 <21020073@vnu.edu.vn>
2023-11-08 00:02:08 +0700

    Create home_page.dart

---
+++ signlingo/lib/src/resources/home/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:signlingo/src/resources/home/unit.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      // height: double.infinity,
+      color: Colors.grey.shade300,
+      child: Unit(unit: 1),
+    );
+  }
+}
