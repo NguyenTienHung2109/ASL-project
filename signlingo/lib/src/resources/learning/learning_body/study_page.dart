a5b2f65ac81f555e1f3f79080baffe4ee5ddc2ec
hoang1803 <21020073@vnu.edu.vn>
2023-11-13 08:30:23 +0700

    Create learning page

---
+++ signlingo/lib/src/resources/learning/learning_body/study_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class StudyPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class StudyPageState extends State<StudyPage> {
+  @override
+  Widget build(Object context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
