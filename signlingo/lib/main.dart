d988856ebf151b4cab926e4c3acf2790db899383
21020541_NguyenTienHung <100121387+NguyenTienHung2109@users.noreply.github.com>
2023-11-15 02:24:47 +0700

    add firebase

--- signlingo/lib/main.dart
+++ signlingo/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:signlingo/src/resources/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-
-void main() {
+void main() async{
   // runApp(const MyApp());
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
