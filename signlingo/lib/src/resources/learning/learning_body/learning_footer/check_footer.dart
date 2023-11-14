5a7b3d60173f6dfdd67812d30680c8d4c2653af9
hoang1803 <21020073@vnu.edu.vn>
2023-11-15 02:01:28 +0700

    Create check_footer.dart

---
+++ signlingo/lib/src/resources/learning/learning_body/learning_footer/check_footer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CheckFooter extends StatelessWidget {
+  late Function checkLesson;
+
+  CheckFooter({super.key, required this.checkLesson});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      height: 100,
+      child: Center(
+        child: Container(
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.orange[300]!, Colors.orange[800]!],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+              color: Colors.amber.shade900,
+              border: Border.all(
+                width: 0.1,
+              ),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            margin: EdgeInsets.only(bottom: 20),
+            child: Center(
+                child: TextButton(
+              child: const Text(
+                "CHECK",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              onPressed: () {
+                checkLesson();
+              },
+            ))),
+      ),
+    );
+  }
+}
