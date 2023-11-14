b998872afdc228829e12a41c105cb31a36d1d4b6
hoang1803 <21020073@vnu.edu.vn>
2023-11-15 02:01:23 +0700

    Create learning_bloc.dart

---
+++ signlingo/lib/src/bloc/learning_bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:camera/camera.dart';
+import 'package:signlingo/src/validators/validations.dart';
+
+class LearningBloc {
+  StreamController _checkController = new StreamController();
+  Stream get checkStream => _checkController.stream;
+
+  bool isCorrectVideo(XFile file, String name) {
+    if (!Validations.isCorrectVideo(file, name)) {
+      _checkController.sink.addError("Incorrect!");
+      return false;
+    }
+    _checkController.sink.add("Correct!");
+    return true;
+  }
+
+  void dispose() {
+    _checkController.close();
+  }
+}
